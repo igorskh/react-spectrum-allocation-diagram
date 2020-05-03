b509c87353b8dc46603ccdf19e09658997a1c428
Igor Kim <igor.skh@gmail.com>
2020-05-03 12:34:28 +0200

    support touch events

--- src/components/detail_components/SubGridElements.js
+++ src/components/detail_components/SubGridElements.js
@@ -7,17 +7,33 @@ function SubGridElements({
     if (!step || !bw) {
         return <></>;
     }
+    const onTouchMoveEnd = (e) => {
+        const endTarget = document.elementFromPoint(
+            e.changedTouches[0].clientX,
+            e.changedTouches[0].clientY
+        );
+        try {
+            onMouseUp(parseInt(endTarget.getAttribute("tag")));
+        } catch {
+            console.error("Unable to detect end target of onTouchMoveEnd", endTarget);
+        }
+    };
 
     const elements = [];
     for (let i = 0; i < parseInt(bw / step); i++) {
         const selected = i >= startID && i <= endID;
         elements.push(
             <div
+                onTouchStart={() => onMouseDown(i)}
+                onTouchEnd={onTouchMoveEnd}
+                onTouchMove={onTouchMoveEnd}
+
                 onMouseDown={() => onMouseDown(i)}
                 onMouseUp={() => onMouseUp(i)}
                 onMouseMove={() => onMouseMove(i)}
                 className={`sg-subgrid-element ${selected ? "selected" : ""}`}
-                key={`sg-subgrid-${i}`}>
+                key={`sg-subgrid-${i}`}
+                tag={i}>
                 {step}
             </div>
         );
