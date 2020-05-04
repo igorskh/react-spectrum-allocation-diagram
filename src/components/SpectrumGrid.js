abda56a9027fe9239a356a361b69fa4b1b5ab2f1
Igor Kim <igor.skh@gmail.com>
2020-05-04 10:43:21 +0200

    make diagram scrollable for small screen devices

--- src/components/SpectrumGrid.js
+++ src/components/SpectrumGrid.js
@@ -5,16 +5,18 @@ import SpectrumGridElement from "./SpectrumGridElement";
 import "./SpectrumGrid.scss";
 
 function SpectrumGrid({ data, onElementClick, selectedItemID }) {
-    return <div className="sg-grid">
-        {data.map((e, i) => (
-            <SpectrumGridElement
-                id={i}
-                key={i}
-                selected={i === selectedItemID}
-                element={e}
-                onClick={onElementClick}
-            />
-        ))}
+    return <div className="sg-grid-container">
+        <div className="sg-grid">
+            {data.map((e, i) => (
+                <SpectrumGridElement
+                    id={i}
+                    key={i}
+                    selected={i === selectedItemID}
+                    element={e}
+                    onClick={onElementClick}
+                />
+            ))}
+        </div>
     </div>;
 }
 
